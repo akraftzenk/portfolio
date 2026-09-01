09f35c2a4f8366f30afe43af2e8f90acce659b49
Aidan Kraftzenk <akraftzenk@gmail.com>
2026-09-01 00:01:35 -0500

    vite page links

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "projects.html"),
+        //experience: resolve(__dirname, 'experience.html'),
+        // one entry per html file
+      },
+    },
+  },
 });
